8146b06ea9245e4c70a949bda874147c89625e5b
himanshu <himanshusaini0345@outlook.com>
2026-04-21 12:17:36 +0530

    chore: implement branch-specific deployment for main

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,21 +1,21 @@
 module.exports = {
   apps: [
     {
-      name: "dev-backend",
+      name: "prod-backend",
       script: "server.js",
       cwd: "./backend",
       env: {
-        PORT: 4000,
-        MONGO_URI: "mongodb://127.0.0.1:27017/todo_dev",
+        PORT: 4002,
+        MONGO_URI: "mongodb://127.0.0.1:27017/todo_prod",
       }
     },
     {
-      name: "dev-frontend",
+      name: "prod-frontend",
       script: "serve.js",
       cwd: "./frontend",
       env: {
-        PORT: 5000,
-        BUILD_FOLDER: "dist"
+        PORT: 5002,
+        BUILD_FOLDER: "build-prod-v2"
       }
     }
   ]
